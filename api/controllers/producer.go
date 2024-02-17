9a2ff596d826a496146380845d9a6aef13c5ac6b
rinto11804 <rinto11804@gmail.com>
2024-02-17 08:41:13 +0530

    login producer

--- api/controllers/producer.go
+++ api/controllers/producer.go
@@ -5,7 +5,40 @@ import (
 	"github.com/akhil-is-watching/enrut_backend_api/storage"
 	"github.com/akhil-is-watching/enrut_backend_api/types"
 	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
 )
+
+func LoginProducer(c *fiber.Ctx) error {
+	var input types.ProducerLoginInput
+
+	if err := c.BodyParser(&input); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": true,
+			"data":  err.Error(),
+		})
+	}
+
+	producerRepo := repository.NewProducerRepository(storage.GetDB())
+	producer, err := producerRepo.GetByEmail(input)
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": true,
+			"data":  err.Error(),
+		})
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(producer.Password), []byte(input.Password)); err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": true,
+			"data":  err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"error": false,
+		"data":  "success",
+	})
+}
 
 func CreateProducer(c *fiber.Ctx) error {
 	var input types.ProducerCreateInput
--- api/models/producer.go
+++ api/models/producer.go
@@ -3,8 +3,9 @@ package models
 import "github.com/google/uuid"
 
 type Producer struct {
-	ID     uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
-	Name   string
-	Email  string `gorm:"unique"`
-	Assets []Asset
+	ID       uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+	Name     string
+	Email    string `gorm:"unique"`
+	Password string
+	Assets   []Asset
 }
